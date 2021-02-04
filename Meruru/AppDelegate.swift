46de0b8c3d62d77b902c5c052308d5306846967a
takuoaoi <>
2021-02-05 02:49:13 +0900

    ・ワンセグを除外・メニューバーからチャンネル切り替え・url schemeで開けるように（EPGStation用）

--- Meruru/AppDelegate.swift
+++ Meruru/AppDelegate.swift
@@ -10,43 +10,59 @@ import Cocoa
 import VLCKit
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate, VLCMediaPlayerDelegate {
     
     @IBOutlet weak var window: NSWindow!
-    @IBOutlet weak var statusTextField: NSTextField!
-    @IBOutlet weak var servicesComboBox: NSComboBox!
     @IBOutlet weak var videoWrapView: NSView!
+    @IBOutlet weak var channelsMenu: NSMenu!
     
     var mirakurun: MirakurunAPI!
-    
     var player: VLCMediaPlayer!
     var services: [Service] = []
     var currentService: Service?
+    var selectedService: Service?
+    var receivedServiceId: Int?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         initUI()
         
+        connectToMirakurun()
+    }
+    
+    func connectToMirakurun() {
         guard let mirakurunPath = AppConfig.shared.currentData?.mirakurunPath ?? promptMirakurunPath() else {
             showErrorAndQuit(error: NSError(domain: "invalid mirakurun path", code: 0))
             return
         }
-
         mirakurun = MirakurunAPI(baseURL: URL(string: mirakurunPath + "/api")!)
         mirakurun.fetchStatus { result in
             switch result {
-            case .success(let status):
+            case .success:
                 AppConfig.shared.currentData?.mirakurunPath = mirakurunPath
-                DispatchQueue.main.async {
-                    self.statusTextField.stringValue = "Mirakurun: v" + status.version
-                }
                 self.mirakurun.fetchServices { result in
                     switch result {
                     case .success(let services):
                         self.services = services
-                        DispatchQueue.main.async {
-                            self.servicesComboBox.addItems(withObjectValues: services.map { $0.name })
-                            self.servicesComboBox.selectItem(at: 0)
+                        // ワンセグなどを除去
+                        self.services = self.services.enumerated().compactMap{
+                            if $0 == 0 {
+                                return $1
+                            }
+                            if self.services[$0].networkId != self.services[$0 - 1].networkId {
+                                return $1
+                            }
+                            return nil
                         }
+                        DispatchQueue.main.async {
+                            self.services.forEach { (service) in
+                                self.channelsMenu.addItem(withTitle: service.name, action: #selector(self.onClickChannelMenuItem), keyEquivalent: "")
+                            }
+                        }
+                        
+                        if !self.openReceivedService() {
+                            self.selectService(selectedService: self.services[0])
+                        }
+                        
                     case .failure(let error):
                         self.showErrorAndQuit(error: error)
                     }
@@ -58,6 +74,27 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
         }
     }
     
+    
+    func application(_ application: NSApplication, open urls: [URL]) {
+        let url = urls[0]
+        let id = Int(url.path.components(separatedBy: "/")[4])
+        
+        self.receivedServiceId = id
+        openReceivedService()
+    }
+    
+    func openReceivedService() -> Bool {
+        guard let id = self.receivedServiceId else {
+            return false
+        }
+        let targetService = self.services.first{$0.id == id}
+        guard let service = targetService else {
+            return false
+        }
+        self.selectService(selectedService: service)
+        return true
+    }
+    
     func showErrorAndQuit(error: Error) {
         let alert = NSAlert(error: error)
         alert.runModal()
@@ -65,8 +102,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
     }
     
     func initUI() {
-        servicesComboBox.delegate = self
-
         let videoView = VLCVideoView(frame: NSRect(x: 0, y: 0, width: videoWrapView.frame.width, height: videoWrapView.frame.height))
         videoView.autoresizingMask = [.width, .height]
         videoView.fillScreen = true
@@ -74,6 +109,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
         videoWrapView.addSubview(videoView)
 
         player = VLCMediaPlayer(videoView: videoView)
+        player.delegate = self
+        
+        self.window.level = .floating
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
@@ -94,9 +132,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
         return nil
     }
     
-    func comboBoxSelectionDidChange(_ notification: Notification) {
-        let selectedService = services[servicesComboBox.indexOfSelectedItem]
-        debugPrint(selectedService)
+    func selectService(selectedService: Service) {
         currentService = selectedService
         mirakurun.fetchPrograms(service: selectedService) { result in
             switch result {
@@ -105,16 +141,18 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
                     return
                 }
                 DispatchQueue.main.async {
-                    self.window?.title = "Meruru - \(program.name) - \(selectedService.name)"
+                    self.window?.title = "\(program.name) - \(selectedService.name)"
                 }
             case .failure(_):
                 return
             }
         }
-        player.stop()
-        let media = VLCMedia(url: mirakurun.getStreamURL(service: selectedService))
-        player.media = media
-        player.play()
+        DispatchQueue.main.async {
+            self.player.stop()
+            let media = VLCMedia(url: self.mirakurun.getStreamURL(service: selectedService))
+            self.player.media = media
+            self.player.play()
+        }
     }
     
     func getNowProgram(programs: [Program]) -> Program? {
@@ -130,6 +168,29 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSComboBoxDelegate {
             let alert = NSAlert(error: err)
             alert.runModal()
         }
+    }
+    
+    private final func fixVideoSize(){
+        let previousFrame = self.window.frame
+        let frame = NSRect(x: previousFrame.minX, y: previousFrame.minY, width: previousFrame.width + 0.1, height: previousFrame.height)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.window.setFrame(frame, display: true, animate: false)
+        }
+    }
+    
+    func mediaPlayerStateChanged(_ aNotification: Notification!) {
+        if self.player.state == .playing {
+            fixVideoSize()
+        }
+    }
+    
+    @objc func onClickChannelMenuItem(_ sender: NSMenuItem) {
+        let name = sender.title
+        let selectedChannelIndex = self.services.firstIndex { (service) -> Bool in
+            return service.name == name
+        }
+        self.selectService(selectedService: self.services[selectedChannelIndex!])
     }
 }
 
